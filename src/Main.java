42f6c17ec346da73146980e2cc0273fca7ac54c5
Stikender <nadik17@bk.ru>
2022-10-26 15:09:35 +0800

    Exercise 1.1-1.3; 2.1-2.3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-    System.out.println("Exercise 1");
+    System.out.println("Exercise 1.1");
         int age1 = 15;
         if (age1 >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
@@ -11,17 +11,15 @@ public class Main {
             System.out.println("Тебе еще нет 18 лет, нужно немного подождать.");
         }
 
-        System.out.println("Exercise 2");
+        System.out.println("Exercise 1.2");
         int age2 = 19;
-        if (age2 <=7) {
-            System.out.println("Ребенок еще мал для обучения в образовательных учреждениях.");
-        }
 
-        if (age2 >= 7 && age2 <= 17) {
+
+        if (age2 >= 7 && age2 < 18) {
             System.out.println("Ребенок школьник");
         }
 
-        if (age2 >= 18 && age2 <= 23) {
+        if (age2 >= 18 && age2 < 24) {
             System.out.println("Человек студент.");
         }
 
@@ -29,8 +27,8 @@ public class Main {
             System.out.println("Человек закончил Университет, можно искать работу.");
         }
 
-        System.out.println("Exercise 3");
-        int placeTrain = 120;
+        System.out.println("Exercise 1.3");
+        int placeTrain = 30;
 
         if (placeTrain < 102 && placeTrain <= 60) {
             System.out.println("В вагоне имеются сидячие и стоячие места.");
@@ -39,6 +37,37 @@ public class Main {
             System.out.println("В вагоне имеются только стоячие места.");
         }
         if (placeTrain > 102) {
+            System.out.println("В вагоне нет совбодных мест.");
+        }
+
+        System.out.println("Exercise 2.1");
+        if (age1 >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        else  {
+            System.out.println("Тебе еще нет 18 лет, нужно немного подождать.");
+        }
+
+        System.out.println("Exercise 2.2");
+
+        if (age2 >= 7 && age2 < 18) {
+            System.out.println("Ребенок школьник");
+        } else if (age2 >= 18 && age2 < 24) {
+            System.out.println("Человек студент.");
+        }
+        else {
+            System.out.println("Человек закончил Университет, можно искать работу.");
+        }
+
+        System.out.println("Exercise 2.3");
+
+        if (placeTrain < 102 && placeTrain <= 60) {
+            System.out.println("В вагоне имеются сидячие и стоячие места.");
+        }
+        else if (placeTrain < 102 && placeTrain > 60) {
+            System.out.println("В вагоне имеются только стоячие места.");
+        }
+        else {
             System.out.println("В вагоне нет совбодных мест.");
         }
     }
